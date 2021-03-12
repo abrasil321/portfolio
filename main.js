5db6c8ce459cfe7a41dd93e66080d39ae8d7af90
Alex Brasil <alexfbrazil123@gmail.com>
2021-03-11 21:00:28 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -2,46 +2,46 @@
 
 function generator1(){
     const links = [
-        {
-          label: "Week1",
-          url: "https://github.com/abrasil321/WWD330/tree/main/js/week01"
-        },
-        {
-          label: "Week2",
-          url: "https://github.com/abrasil321/WWD330/blob/main/js/week2"
-        },
-        {
-          label: "Week3",
-          url: "https://github.com/abrasil321/WWD330/tree/main/js/week3"
-        },
-        {
-          label: "Week4",
-          url: "https://github.com/abrasil321/WWD330/tree/main/js/week4"
-        },
-        {
-          label: "Week5",
-          url: "https://github.com/abrasil321/WWD330/tree/main/js/week5"
-        },
-        {
-          label: "Week6",
-          url: "https://github.com/abrasil321/WWD330/tree/master/js/ToDo%20app"
-        },
-        {
-          label: "Week7",
-          url: "https://github.com/abrasil321/WWD330/tree/master/js/week7"
-        },
-        {
-          label: "Week8",
-          url: "https://github.com/abrasil321/WWD330/tree/master/js/week8"
-        },
-        {
-          label: "Week9",
-          url: "https://github.com/abrasil321/WWD330/tree/master/js/week9"
-        }
-      ];
+      {
+        label: "Week1",
+        url: "https://github.com/abrasil321/WWD330/tree/main/js/week01"
+      },
+      {
+        label: "Week2",
+        url: "https://github.com/abrasil321/WWD330/blob/main/js/week2"
+      },
+      {
+        label: "Week3",
+        url: "https://github.com/abrasil321/WWD330/tree/main/js/week3"
+      },
+      {
+        label: "Week4",
+        url: "https://github.com/abrasil321/WWD330/tree/main/js/week4"
+      },
+      {
+        label: "Week5",
+        url: "https://github.com/abrasil321/WWD330/tree/main/js/week5"
+      },
+      {
+        label: "Week6",
+        url: "https://github.com/abrasil321/WWD330/tree/master/js/ToDo%20app"
+      },
+      {
+        label: "Week7",
+        url: "https://github.com/abrasil321/WWD330/tree/master/js/week7"
+      },
+      {
+        label: "Week8",
+        url: "https://github.com/abrasil321/WWD330/tree/master/js/week8"
+      },
+      {
+        label: "Week9",
+        url: "https://github.com/abrasil321/WWD330/tree/master/js/week9"
+      }
+    ];
     let display = "";
     for(let i = 0; i < links.length; i++){
-      display += "<li><a href='" + links[i].url + "'>" + links[i].label + "</a></li><br>";
+      display += "<li><a href='" + links[i].url + "' rel='" + links[i].url + "'>" + links[i].label + "</a></li><br>";
     }
     document.getElementById("links").innerHTML = display;
 }
